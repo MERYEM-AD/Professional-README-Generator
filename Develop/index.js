bec3b878f533e84850af6b39983afb130443efc7
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-11-10 20:15:45 -0500

    Add initial Directories

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+
+//requte fs and inqurer 
+
+// TODO: Create an array of questions for user input
+const questions = []; //  array of object 
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {} //standart format of readme file
+
+// Function call to initialize app
+init();
+
+
